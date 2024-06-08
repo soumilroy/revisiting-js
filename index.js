8f1cc9e9d882b0cebf73b8cdd67d7d0b8b944c24
Soumil Roy <hello@soumilroy.com>
2024-06-08 06:54:57 +0530

    methods

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ import { BankAccount } from "./oop/bank.js";
 
 const account = new BankAccount("John Doe");
 
-console.log(account);
+// console.log(account);
+
+const currentBalance = account.getCurrentBalance();
+
+console.log(`Current balance: ${currentBalance}`);
+
+account.showTransactionHistory();
+
+account.deposit(4000);
+
+account.showTransactionHistory();
+
+account.withdraw(1000);
+
+account.showTransactionHistory();
+
+console.log(`Current balance: ${account.getCurrentBalance()}`);
+
+account.withdraw(1390);
+
+account.showTransactionHistory();
+console.log(`Current balance: ${account.getCurrentBalance()}`);
--- oop/bank.js
+++ oop/bank.js
@@ -8,14 +8,73 @@
  */
 
 export class BankAccount {
+  // constructor used for validating and assigning properties
   constructor(name) {
     if (!name) {
       throw new Error("Name is required to create an account");
     }
 
-    this.id = Math.floor(Math.random() * 1000000);
+    this.accountNumber = Math.floor(Math.random() * 1000000);
     this.name = name;
     this.balance = 0;
     this.transactionHistory = [];
+    this.currency = "INR";
+    this.currencySymbol = "₹";
+  }
+
+  getCurrentBalance() {
+    return this.balance;
+  }
+
+  updateName(name) {
+    this.name = name;
+  }
+
+  deposit(amount) {
+    if (typeof amount !== "number" || amount <= 0) {
+      throw new Error("Invalid amount to deposit. Please try again.");
+    }
+
+    this.balance += amount;
+    this.transactionHistory.push({
+      transactionId: Math.floor(Math.random() * 100),
+      type: "credit",
+      amount: amount,
+      currency: this.currency,
+      currencySymbol: this.currencySymbol,
+      date: new Date()
+    });
+  }
+
+  withdraw(amount) {
+    if (typeof amount !== "number" || amount <= 0) {
+      console.log("Invalid amount to withdraw. Please try again.");
+      return;
+    }
+
+    if (this.balance < amount) {
+      console.log("Insufficient balance. Please try again.");
+      return;
+    }
+
+    this.balance -= amount;
+
+    this.transactionHistory.push({
+      transactionId: Math.floor(Math.random() * 100),
+      type: "debit",
+      amount: amount,
+      currency: this.currency,
+      currencySymbol: this.currencySymbol,
+      date: new Date()
+    });
+  }
+
+  showTransactionHistory() {
+    if (!this.transactionHistory.length) {
+      console.log("No transactions found.");
+      return;
+    }
+
+    console.table(this.transactionHistory);
   }
 }
